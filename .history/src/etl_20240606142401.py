dee4a43a8c772f8f77a7b0d1d4c1bd6b4b92dc2f
Rhys Wells <rhyslwells@outlook.com>
2024-06-06 14:24:01 +0100

    setup etl.py - will improve now

---
+++ .history/src/etl_20240606142401.py
@@ -0,0 +1,39 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+def extract_csv(path)-> pd.DataFrame:
+    """ load saved csv to pandas dataframe"""
+    return pd.read_csv(path)
+
+def extract_api(url:str)-> pd.DataFrame:
+    """ Extract data from API"""
+    response = requests.get(url)
+    data = response.json()
+    return pd.DataFrame(data)
+
+
+def transform_GMMC(data:pd.DataFrame) -> pd.DataFrame:
+    """ Transforms the dataset into desired structure and filters"""
+    print(f"Size {data.shape}")
+
+    df=data
+    return df
+
+def load_GMMC(df: pd.DataFrame, name: str) -> None:
+    """ Load data into an SQLite database """
+    disk_engine = create_engine('sqlite:///../data_storage/GMMC/processed_data/' + name + '.db')
+    df.to_sql(name, disk_engine, if_exists='replace')
+
+# Given 
+path = "../data_storage/GMMC/raw_data/GMMC-2020-M.csv"
+name = "GMMC-2020-M"
+
+# Current path: src/etl.py
+data = extract_csv(path)
+df = transform_GMMC(data)
+# Save to data_storage/GMMC with name GMMC-2020-M.db
+load_GMMC(df, name)
+
+
+
--- src/etl.py
+++ src/etl.py
@@ -20,19 +20,20 @@ def transform_GMMC(data:pd.DataFrame) -> pd.DataFrame:
     df=data
     return df
 
-def load(df:pd.DataFrame,name)-> None:
+def load_GMMC(df: pd.DataFrame, name: str) -> None:
     """ Load data into an SQLite database """
-    disk_engine = create_engine('sqlite:///my_lite_store.db')
+    disk_engine = create_engine('sqlite:///../data_storage/GMMC/processed_data/' + name + '.db')
     df.to_sql(name, disk_engine, if_exists='replace')
 
 # Given 
-path= "data_storage\raw_data\GMMC_monitoring\GMMC-2020-M.csv"
-total_path="..\\"+ path
-name="GMMC-2020-M"
+path = "../data_storage/GMMC/raw_data/GMMC-2020-M.csv"
+name = "GMMC-2020-M"
+
+# Current path: src/etl.py
 data = extract_csv(path)
 df = transform_GMMC(data)
-# Save to data_storage\GMMC with name GMMC-2020-M.db
-load(df)
+# Save to data_storage/GMMC with name GMMC-2020-M.db
+load_GMMC(df, name)
 
 
 
